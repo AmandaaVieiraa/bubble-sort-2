458b4cd3a03f1ff879f4a615fdd10fd319783bdf
AMANDA VIEIRA ROCHA <00001114756222sp@aluno.educacao.sp.gov.br>
2023-12-05 11:08:19 -0300

    bghyjgdu

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let vetorInicial=[9,5,7,2,6,1,3,0,4,8]
-let vetor1=[5,9,7,2,6,1,3,0,4,8]
-let vetor2=[2,5,7,9,6,1,3,0,4,8]
-let vetor3=[1,2,5,7,9,6,3,0,4,8]
-let vetor4=[1,2,5,7,6,9,3,0,4,8]
-let vetor5=[1,2,5,6,7,9,3,0,4,8]
-let vetor6=[1,2,3,5,6,7,9,0,4,8]
-let vetor7=[0,1,2,3,5,6,7,9,4,8]
-let vetor8=[0,1,2,3,4,5,6,7,9,8]
-let vetor9=[0,1,2,3,4,5,6,7,8,9]
+
+
+let vetorInicial=[ 1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59]
+let vetorFinal = [];
+for(let i = 0; i < vetorInicial.length; i++){
+   
+    vetorInicial[i] % 2 == 1
+    vetorFinal.push(vetorInicial[i])
+}
+
+console.log(vetorFinal)
